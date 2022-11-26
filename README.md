63b01123dc9b9cb642cf7b84dd452f764a6dcab8
srg6 <srg6@sfu.ca>
2022-11-25 16:51:48 -0800

    Deployment

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# RSA_CMPT732_Project
-Project for CMPT 732 Course SFU by Team RSA 
+These are commits by RSA
--- app.py
+++ app.py
@@ -1,95 +1,206 @@
 import dash                     # pip install dash
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 from dash import dcc, html
 import plotly.express as px     # pip install plotly==5.2.2
 
 import pandas as pd             # pip install pandas
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+from sqlalchemy import create_engine
+import pymysql
+import pickle
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 server = Flask(__name__)
-app = dash.Dash(__name__, server = server, external_stylesheets=external_stylesheets, suppress_callback_exceptions= True)
-app.server.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app = dash.Dash(__name__, server=server,
+                external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
 
-app.server.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://bgmfqpvaznkong:28dd0e1b6da38e2e47bd8b33f72169c233264c732c26c2b9b88bf7c7d21bd819@ec2-35-173-91-114.compute-1.amazonaws.com:5432/d8nf5i3c87jp03"
 
 with server.app_context():
-    db = SQLAlchemy(app.server)
-    df = pd.read_sql_table("cleaned_data", con = db.engine)
+    sqlEngine = create_engine(
+        "mysql+pymysql://admin:rsa12345@opvd.ceeldi77ur7c.us-west-2.rds.amazonaws.com", pool_recycle=3600)
+    dbConnection = sqlEngine.connect()
+    type_num_df = pd.read_sql(
+        "select * from openvancoverpolicedata.opvd_type_num_cleaned_data order by year", dbConnection)
+    type_num_df.head()
+    neighbourhood_num_df = pd.read_sql(
+        "select * from openvancoverpolicedata.opvd_neighbourhood_num_cleaned_data order by year", dbConnection)
+    month_num_df = pd.read_sql(
+        "select * from openvancoverpolicedata.opvd_month_num_cleaned_data order by year", dbConnection)
+    hour_num_df = pd.read_sql(
+        "select * from openvancoverpolicedata.opvd_hour_num_cleaned_data order by year", dbConnection)
+    type_neighbourhood_num_df = pd.read_sql(
+        "select * from openvancoverpolicedata.opvd_type_neighbourhood_num_df_cleaned_data order by year", dbConnection)
+    districtwise_highcrimerate_df = pd.read_sql(
+        "select * from openvancoverpolicedata.opvd_districtwise_highcrimerate", dbConnection)
+    dbConnection.close()
 
 app.layout = html.Div([
     html.H1("Analytics Dashboard of Vancover Police Department (Dash Plotly)", style={
             "textAlign": "center"}),
+    html.Hr(),
+    html.Div(children=[
+
+        html.Span(children=[
+            html.P("Enter Year:"),
+            html.Div(
+                dcc.Input(
+                    id="input_year",
+                    type="number",
+                    placeholder="year",
+                ))
+        ], style={"display": "inline-block", "margin": "5px"}),
+
+        html.Span(children=[
+            html.P("Enter Month:"),
+            html.Div(
+                dcc.Input(
+                    id="input_month",
+                    type="number",
+                    placeholder="month",
+                )),
+        ], style={"display": "inline-block", "margin": "5px"}),
+
+        html.Span(children=[
+            html.P("Enter Hour:"),
+            html.Div(
+                dcc.Input(
+                    id="input_hour",
+                    type="number",
+                    placeholder="hour",
+                )),
+        ], style={"display": "inline-block", "margin": "5px"}),
+
+        html.Span(children=[
+            html.P("Enter Neighbourhood:"),
+            html.Div(
+                dcc.Input(
+                    id="input_neighbourhood",
+                    type="text",
+                    placeholder="neighbourhood",
+                )),
+        ], style={"display": "inline-block", "margin": "5px"}),
+
+        html.Span(children=[
+            html.P("Enter Hundred Block:"),
+            html.Div(
+                dcc.Input(
+                    id="input_hundred_block",
+                    type="text",
+                    placeholder="hundred block",
+                )),
+        ], style={"display": "inline-block", "margin": "5px"}),
+
+    ], className="inline"),
+
+    html.Br(),
+    html.Button('Submit', id='submit-val', n_clicks=0,
+                style={"display": "inline-block", "margin": "5px"}),
+    html.Hr(),
+    html.Div(id="predicted-div", children=[]),
+
     html.Hr(),
     html.P("Choose Year:"),
     html.Div(html.Div([
         dcc.Dropdown(id='crime-type', clearable=False,
                      value=2003,
                      options=[{'label': x, 'value': x} for x in
-                              df.sort_values(by=['year'])["year"].unique()]),
+                              type_num_df.sort_values(by=['year'])["year"].unique()]),
     ], className="two columns"), className="row"),
-
+    html.Hr(),
+    dcc.Interval(
+        id='interval-component',
+        interval=1*1000*3600*60,  # in milliseconds
+        n_intervals=0
+    ),
     html.Div(id="output-div", children=[]),
 ])
 
 
+@app.callback(Output(component_id="predicted-div", component_property="children"),
+              Input('submit-val', 'n_clicks'),
+              State('input_year', 'value'),
+              State('input_month', 'value'),
+              State('input_hour', 'value'),
+              State('input_neighbourhood', 'value'),
+              State('input_hundred_block', 'value'),
+              )
+def make_prediction(submit_val, input_year, input_month, input_hour, input_neighbourhood, input_hundred_block):
+    if input_year != None and input_month != None and input_hour != None and input_neighbourhood != None and input_hundred_block != None:
+        pickled_model = pickle.load(open('model.pkl', 'rb'))
+        pickled_target_label = pickle.load(open('target_label.pkl', 'rb'))
+        pickled_neighbourhood_label = pickle.load(
+            open('neighbourhood_label.pkl', 'rb'))
+        pickled_hundred_block_label = pickle.load(
+            open('hundred_block_label.pkl', 'rb'))
+        if input_neighbourhood not in pickled_neighbourhood_label and input_hundred_block not in pickled_hundred_block_label:
+            return [
+                html.H2("Wrong Input", style={
+                    "textAlign": "center"}),
+            ]
+        predicted_label = pickled_model.predict(
+            [[input_year, input_month, input_hour, pickled_neighbourhood_label[input_neighbourhood], pickled_hundred_block_label[input_hundred_block]]])
+
+        key = [k for k, v in pickled_target_label.items() if v ==
+               predicted_label]
+        return [
+            html.H2(key[0].capitalize(), style={
+                "textAlign": "center"}),
+        ]
+    else:
+        return [
+            html.H2("", style={
+                "textAlign": "center"}),
+        ]
+
+
 @app.callback(Output(component_id="output-div", component_property="children"),
               Input(component_id="crime-type", component_property="value"),
+              Input(component_id="interval-component",
+                    component_property="n_intervals")
               )
-def make_bars(animal_chosen):
+def make_bars(year_given, interval):
     # HISTOGRAM
-    df_hist = df[df["year"] == animal_chosen]
-    fig_hist = px.histogram(df_hist, y="type", text_auto=True,
+    type_num_df_hist = type_num_df[type_num_df.year == year_given]
+    type_num_df_hist.head()
+    fig_hist = px.histogram(type_num_df_hist, y="type", x='count', text_auto=True,
                             title="Total Number of Crime By Type")
     fig_hist.update_yaxes(categoryorder="total ascending")
 
     # HISTOGRAM
-    fig_strip = px.histogram(df_hist, x="neighbourhood",
+    neighbourhood_num_df_hist = neighbourhood_num_df[neighbourhood_num_df.year == year_given]
+    fig_strip = px.histogram(neighbourhood_num_df_hist, x="neighbourhood", y="count",
                              text_auto=True, title="Total Crime By Neighbourhood")
     fig_strip.update_xaxes(categoryorder="total descending")
 
     # HISTOGRAM
-    df_line_stacked = df_hist.groupby(
-        ["type", "neighbourhood"]).size().reset_index(name="count")
-    fig_stacked = px.bar(df_line_stacked, x="neighbourhood", y="count",
+    type_neighbourhood_num_df_hist = type_neighbourhood_num_df[
+        type_neighbourhood_num_df.year == year_given]
+    fig_stacked = px.bar(type_neighbourhood_num_df_hist, x="neighbourhood", y="count",
                          color="type", title="Stacked Type of Crime by Neighbourhood")
 
-    # # SUNBURST
-    # df_sburst = df.dropna(subset=['chip_status'])
-    # df_sburst = df_sburst[df_sburst["intake_type"].isin(
-    #     ["STRAY", "FOSTER", "OWNER SURRENDER"])]
-    # fig_sunburst = px.sunburst(
-    #     df_sburst, path=["animal_type", "intake_type", "chip_status"])
-
-    # # Empirical Cumulative Distribution
-    # df_ecdf = df[df["animal_type"].isin(["DOG", "CAT"])]
-    # fig_ecdf = px.ecdf(df_ecdf, x="animal_stay_days", color="animal_type")
-
     # # LINE CHART
-    df_line = df_hist.groupby(["month"]).size().reset_index(name="count")
-    fig_line_month = px.line(df_line, x="month", y="count",
+    month_num_df_line = month_num_df[month_num_df.year == year_given].sort_values(
+        by='month')
+    fig_line_month = px.line(month_num_df_line, x="month", y="count",
                              markers=True, text="count", title="Total Crime By Month")
     fig_line_month.update_traces(textposition="bottom right")
 
     # # LINE CHART
-    df_line = df_hist.groupby(["hour"]).size().reset_index(name="count")
-    fig_line_hour = px.line(df_line, x="hour", y="count",
+    hour_num_df_line = hour_num_df[hour_num_df.year ==
+                                   year_given].sort_values(by='hour')
+    fig_line_hour = px.line(hour_num_df_line, x="hour", y="count",
                             markers=True, text="count", title="Total Crime By Hour")
     fig_line_hour.update_traces(textposition="bottom right")
+
+    # Histogram
+    fig_histogram = px.histogram(districtwise_highcrimerate_df, x="District", y=["2020", "2021", "2022"], barmode='group', title="Total Crime Count Per District for the years 2020-2022")
 
     return [
         html.Div([
             html.Div([dcc.Graph(figure=fig_hist)], className="six columns"),
             html.Div([dcc.Graph(figure=fig_strip)], className="six columns"),
         ], className="row"),
-        # html.H2("All Animals", style={"textAlign": "center"}),
-        # html.Hr(),
-        # html.Div([
-        #     html.Div([dcc.Graph(figure=fig_sunburst)],
-        #              className="six columns"),
-        #     html.Div([dcc.Graph(figure=fig_ecdf)], className="six columns"),
-        # ], className="row"),
         html.Div([
             html.Div([dcc.Graph(figure=fig_stacked)],
                      className="twelve columns"),
@@ -102,6 +213,19 @@ def make_bars(animal_chosen):
             html.Div([dcc.Graph(figure=fig_line_hour)],
                      className="twelve columns"),
         ], className="row"),
+        # html.Div([
+        #     html.Div([dcc.Graph(figure=fig_histogram)], className="six columns"),
+        # ], className="row"),
+        html.Div([
+            html.Div([dcc.Graph(figure=fig_histogram)]),
+        ], className="row"),
+        
+        html.H5("The Folium Map depicts the most Crime Hit Hundred_Blocks in District 2(The Least Total Crime Count district since 2020-2022)"),
+        html.Div([
+            html.Iframe(id='map', srcDoc=open("District2Map.html",
+                        "r").read(), width='80%', height='600')
+        ], className="row"),
+        
     ]
 
 
